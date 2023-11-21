2451c490647b2f69c009ffddf36e8cdea1d210c3
kr.mao <kr.mao@qq.com>
2023-11-21 15:59:16 +0800

    init +4

--- src/error.vue
+++ src/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="display: flex; flex-direction: column">
-    <NuxtLayout name="error-layout" title="From Error">
-      404
+    <NuxtLayout name="error" title="From Error">
+      人在旅途，难免迷途~
     </NuxtLayout>
   </div>
 </template>
