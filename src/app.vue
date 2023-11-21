c4002d553bce3adb63253a88ff8a8ad6345b4624
kr.mao <kr.mao@qq.com>
2023-11-21 14:11:02 +0800

    init +2

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,9 @@
+<!-- https://nuxt.com.cn/docs/getting-started/views#%E9%A1%B5%E9%9D%A2 -->
 <template>
+  <p>一些在所有页面之间共享的默认布局内容 - ROOT</p>
   <NuxtLayout>
     <NuxtPage/>
   </NuxtLayout>
 </template>
+<script setup>
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <p>一些在所有页面之间共享的默认布局内容 - INDEX</p>
   <NuxtWelcome/>
 </template>
 <script setup>
