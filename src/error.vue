7557722237cc2869a969cd53deb059cf499d5b98
kr.mao <kr.mao@qq.com>
2023-11-21 16:51:17 +0800

    init +4 404

--- src/error.vue
+++ src/error.vue
@@ -1,9 +1,11 @@
 <template>
   <div style="display: flex; flex-direction: column">
     <NuxtLayout name="error" title="From Error">
-      人在旅途，难免迷途~
+      <h1 v-if="error.statusCode === 404">人在旅途，难免迷途~</h1>
+      <h1 v-else>An error occurred</h1>
     </NuxtLayout>
   </div>
 </template>
 <script setup>
+const {error} = defineProps(['error']);
 </script>
