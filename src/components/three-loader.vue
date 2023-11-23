62a4a290e2fcb535c78da37d8dd06888ff61d429
kr.mao <kr.mao@qq.com>
2023-11-23 12:01:10 +0800

    feat: three.js + 7

--- src/components/three-loader.vue
+++ src/components/three-loader.vue
@@ -1,6 +1,6 @@
 <template>
   <TresCanvas window-size v-bind="gl">
-    <TresPerspectiveCamera :position="[3, 3, 3] as any" />
+    <TresPerspectiveCamera :position="[9, 9, 9] as any" />
     <OrbitControls />
     <Suspense>
       <!--suppress HtmlUnknownTag -->
@@ -14,6 +14,7 @@ import * as THREE from 'three'
 import { OrbitControls } from '@tresjs/cientos'
 // https://threejs.org/docs/#examples/en/loaders/OBJLoader
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js'
 
 const { BasicShadowMap, NoToneMapping, Scene, SRGBColorSpace } = THREE
 
@@ -26,32 +27,42 @@ const gl = {
   toneMapping: NoToneMapping
 }
 const scene = new Scene()
-//
+
 onMounted(() => {
-  const manager = new THREE.LoadingManager()
-  manager.onStart = function (url, itemsLoaded, itemsTotal) {
-    console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
-  }
+  const mtlLoader = new MTLLoader()
+  mtlLoader.setPath('http://localhost:3000/objs/jeep/')
+  mtlLoader.setMaterialOptions({ invertTrProperty: true })
+  mtlLoader.load(
+    'jeep.mtl',
+    (materials) => {
+      console.log('-- onLoad', materials)
 
-  manager.onLoad = function () {
-    console.log('Loading complete!')
-  }
+      materials.preload()
 
-  manager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
-  }
-
-  manager.onError = function (url) {
-    console.log('There was an error loading ' + url)
-  }
-  new OBJLoader(manager).load(
-    'objs/P911GT/Porsche_911_GT2.obj',
-    (object: any) => {
-      console.log('-- onLoad', object)
-      scene.add(object)
+      const objLoader = new OBJLoader()
+      objLoader.setPath('http://localhost:3000/objs/jeep/')
+      objLoader.setMaterials(materials)
+      objLoader.load(
+        'jeep.obj',
+        (object: any) => {
+          console.log('-------- onLoad', object)
+          const mesh = object
+          mesh.scale.set(0.5, 0.5, 0.5)
+          // mesh.position.y = -1
+          // mesh.rotateX(-Math.PI / 2)
+          scene.add(mesh)
+        },
+        (xhr) => {
+          console.log('-------- onProgress ' + (xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error: any) => {
+          console.log('-------- onError', error)
+        }
+      )
+      objLoader.setMaterials(materials)
     },
     (xhr) => {
-      console.log('-- onProgress' + (xhr.loaded / xhr.total) * 100 + '% loaded')
+      console.log('-- onProgress ' + (xhr.loaded / xhr.total) * 100 + '% loaded')
     },
     (error: any) => {
       console.log('-- onError', error)
