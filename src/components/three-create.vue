e88bcb8d7c4b35e3d521b48610f668ab66fc531a
kr.mao <kr.mao@qq.com>
2023-11-23 11:21:58 +0800

    feat: three.js + 66

---
+++ src/components/three-create.vue
@@ -0,0 +1,29 @@
+<template>
+  <TresCanvas window-size v-bind="gl">
+    <TresPerspectiveCamera :position="[3, 3, 3] as any" />
+    <OrbitControls />
+    <Suspense>
+      <!--suppress HtmlUnknownTag -->
+      <primitive :object="meshWithMaterial" />
+    </Suspense>
+  </TresCanvas>
+</template>
+
+<script setup lang="ts">
+import * as THREE from 'three'
+import { OrbitControls } from '@tresjs/cientos'
+
+const { BasicShadowMap, BoxGeometry, Mesh, MeshBasicMaterial, NoToneMapping, SRGBColorSpace } = THREE
+
+const gl = {
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping
+}
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshBasicMaterial({ color: '#fefefe' })
+const meshWithMaterial = new Mesh(geometry, material)
+</script>
--- src/components/three-loader.vue
+++ src/components/three-loader.vue
@@ -1,9 +1,21 @@
+<template>
+  <TresCanvas window-size v-bind="gl">
+    <TresPerspectiveCamera :position="[3, 3, 3] as any" />
+    <OrbitControls />
+    <Suspense>
+      <!--suppress HtmlUnknownTag -->
+      <primitive :object="scene" />
+    </Suspense>
+  </TresCanvas>
+</template>
+
 <script setup lang="ts">
+import * as THREE from 'three'
 import { OrbitControls } from '@tresjs/cientos'
-import { BasicShadowMap, BoxGeometry, Mesh, MeshBasicMaterial, NoToneMapping, Scene, SRGBColorSpace } from 'three'
-import { Vector3 } from 'three/src/math/Vector3'
 // https://threejs.org/docs/#examples/en/loaders/OBJLoader
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
+
+const { BasicShadowMap, NoToneMapping, Scene, SRGBColorSpace } = THREE
 
 const gl = {
   clearColor: '#82DBC5',
@@ -13,38 +25,37 @@ const gl = {
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping
 }
-const geometry = new BoxGeometry(1, 1, 1)
-const material = new MeshBasicMaterial({ color: '#fefefe' })
-// noinspection JSUnusedGlobalSymbols
-// eslint-disable-next-line
-const meshWithMaterial = new Mesh(geometry, material)
 const scene = new Scene()
 //
 onMounted(() => {
-  new OBJLoader().load(
+  const manager = new THREE.LoadingManager()
+  manager.onStart = function (url, itemsLoaded, itemsTotal) {
+    console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+  }
+
+  manager.onLoad = function () {
+    console.log('Loading complete!')
+  }
+
+  manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+  }
+
+  manager.onError = function (url) {
+    console.log('There was an error loading ' + url)
+  }
+  new OBJLoader(manager).load(
     'objs/P911GT/Porsche_911_GT2.obj',
     (object: any) => {
-      console.log('---- ', object)
+      console.log('-- onLoad', object)
       scene.add(object)
     },
     (xhr) => {
-      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      console.log('-- onProgress' + (xhr.loaded / xhr.total) * 100 + '% loaded')
     },
     (error: any) => {
-      console.log('An error happened', error)
+      console.log('-- onError', error)
     }
   )
 })
 </script>
-
-<template>
-  <TresCanvas window-size v-bind="gl">
-    <TresPerspectiveCamera :position="[3, 3, 3] as Vector3" />
-    <OrbitControls />
-    <Suspense>
-      <!--suppress HtmlUnknownTag -->
-      <!--      <primitive :object="meshWithMaterial" />-->
-      <primitive :object="scene" />
-    </Suspense>
-  </TresCanvas>
-</template>
