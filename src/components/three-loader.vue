60d7d9acab2fd1ec91d4256c7400cc9716c441d7
kr.mao <kr.mao@qq.com>
2023-11-23 11:09:44 +0800

    feat: three.js + 4

--- src/components/three-loader.vue
+++ src/components/three-loader.vue
@@ -1,35 +1,57 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { ObjectLoader, Scene } from 'three'
+import { OrbitControls } from '@tresjs/cientos'
+import {
+  BasicShadowMap,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  NoToneMapping,
+  ObjectLoader,
+  Scene,
+  SRGBColorSpace
+} from 'three'
+import { Vector3 } from 'three/src/math/Vector3'
 
-const renderer = ref(null)
-
-const SScene = new Scene()
-const loader = new ObjectLoader()
-// load a resource
-loader.load(
-  // resource URL
-  'objs/龙山大屏-导出.obj',
-  // called when resource is loaded
-  (object) => {
-    SScene.add(object)
-  },
-  // called when loading is in progresses
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-  },
-  // called when loading has errors
-  (error: any) => {
-    console.log('An error happened', error)
-  }
-)
-
-// renderer.render( scene, camera );
+const gl = {
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping
+}
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshBasicMaterial({ color: '#fefefe' })
+// noinspection JSUnusedGlobalSymbols
+// eslint-disable-next-line
+const meshWithMaterial = new Mesh(geometry, material)
+const scene = new Scene()
+//
+onMounted(() => {
+  new ObjectLoader().load(
+    'objs/P911GT/Porsche_911_GT2.obj',
+    (object: any) => {
+      console.log('---- ')
+      scene.add(object.scene)
+    },
+    (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error: any) => {
+      console.log('An error happened', error)
+    }
+  )
+})
 </script>
 
 <template>
-  <Renderer ref="renderer">
-    <Camera :position="{ z: 10 }" />
-    <SScene />
-  </Renderer>
+  <TresCanvas window-size v-bind="gl">
+    <TresPerspectiveCamera :position="[3, 3, 3] as Vector3" />
+    <OrbitControls />
+    <Suspense>
+      <!--suppress HtmlUnknownTag -->
+      <!--      <primitive :object="meshWithMaterial" />-->
+      <primitive :object="scene" />
+    </Suspense>
+  </TresCanvas>
 </template>
