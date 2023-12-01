2266910a23f4f8865c9c3fb18776e1ee94d3bbe4
kr.mao <kr.mao@qq.com>
2023-12-01 19:55:28 +0800

    feat: flb ok +5

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="content-container">
-    <h1>{{ (data as any).length }}</h1>
-    <div>WELCOME TO NUXTJS</div>
-    <van-button type="primary" @click="showToast('toast')"> button</van-button>
-    <VanButton type="success" @click="showNotify('notify')">button</VanButton>
-    <LazyVanButton type="default">lazy button</LazyVanButton>
+  <div class="root-container">
     <!--<three-test />-->
     <!--<three-loader />-->
     <!--    <three-fbx />-->
     <!--    <three-glb />-->
-    <three-glb-custom />
+    <div class="content-3d">
+      <three-glb-custom />
+    </div>
+    <div class="content-container">
+      <div class="head-container">
+        <h1>{{ (data as any).length }}</h1>
+        <div>WELCOME TO NUXTJS</div>
+        <van-button type="primary" @click="showToast('toast')"> button</van-button>
+        <VanButton type="success" @click="showNotify('notify')">button</VanButton>
+        <LazyVanButton type="default">lazy button</LazyVanButton>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -17,8 +23,30 @@ const res = await useAsyncData('post', () => $fetch('http://api.tvmaze.com/searc
 const data = await res.data
 </script>
 <style scoped>
+.root-container {
+  position: relative;
+  background: #fefefe;
+}
+
 .content-container {
   padding: 10px;
-  background: orange;
+  background: transparent;
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  .head-container {
+    padding: 15px;
+    background: #fff9;
+  }
+}
+
+.content-3d {
+  position: absolute;
+  inset: 0;
+  z-index: 0;
 }
 </style>
